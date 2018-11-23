efd6825c2a95b48a5b8ee483c9d4d3a954794c1c
xraybrain <xraybrain@gmail.com>
2018-11-23 03:55:16 +0100

    Application init

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 let mongoURI = "";
 
 if(process.env.NODE_ENV === "production"){
-  mongoURI = "mongodb://xraybrainOnXrayTalk:60534088xraytalk@ds115154.mlab.com:15154/xraytalk-prod";
+  mongoURI = "mongodb://xraybrainOnXraytalk:60534088xraytalk@ds115154.mlab.com:15154/xraytalk-prod";
 } else {
   mongoURI = "mongodb://localhost:27017/xraytalk-dev"
 }
